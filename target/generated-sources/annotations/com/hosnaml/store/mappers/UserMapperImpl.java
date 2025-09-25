6f586b9237260f7fdc140b20e802117066fd406e
hosna <hosna@kth.se>
2025-09-25 11:56:08 +0200

    feat(user): implement UserMapper for converting User entities to UserDto

---
+++ target/generated-sources/annotations/com/hosnaml/store/mappers/UserMapperImpl.java
@@ -0,0 +1,34 @@
+package com.hosnaml.store.mappers;
+
+import com.hosnaml.store.dtos.UserDto;
+import com.hosnaml.store.entities.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-25T11:53:21+0200",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 25 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto toDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String name = null;
+        String email = null;
+
+        id = user.getId();
+        name = user.getName();
+        email = user.getEmail();
+
+        UserDto userDto = new UserDto( id, name, email );
+
+        return userDto;
+    }
+}
