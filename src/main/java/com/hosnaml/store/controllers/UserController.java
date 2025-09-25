9f26abd38ad51bde2aac0e149e2bcb4f1db7be9a
hosna <hosna@kth.se>
2025-09-25 11:20:25 +0200

    feat(user): implement UserDto and update UserRepository and UserController for DTO usage

--- src/main/java/com/hosnaml/store/controllers/UserController.java
+++ src/main/java/com/hosnaml/store/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.hosnaml.store.dtos.UserDto;
 
 @RestController
 @AllArgsConstructor
@@ -17,17 +18,21 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping()
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(user);
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 
 }
--- src/main/java/com/hosnaml/store/dtos/UserDto.java
+++ src/main/java/com/hosnaml/store/dtos/UserDto.java
@@ -1,5 +1,13 @@
 package com.hosnaml.store.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
 
 }
